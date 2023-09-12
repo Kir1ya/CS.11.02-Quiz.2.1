1fa42b888de1e49883e2e50e2162c7af040b520e
Kir1ya <fireboardowo@gmail.com>
2023-09-12 14:32:00 +0800

    Don't understand

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+public static void main(String[] args) {
+}
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+    public static String greeting(String name) {
+        return "Bonjour " + name;
+        System.out.println(greeting);
+        //method doesnt work, dont understand
+    }
+    public static int add(int numThree, int numFour) {
+        return (add + numThree + numFour)*3;
+        int resultThree = int add;
+        System.out.println(add);
+        //method doesnt work, dont understand
+    }
+    public static String printMe(String toBePrinted) {
+        return
+        //method doesnt work, dont understand
+    }
 }
